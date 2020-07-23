3f24a6c02a5ca0737f85e9171617f286524bba40
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-23 09:44:40 -0400

    Add one-to-many relationships and new test

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: 'vegetable', description: 'great vegetable recipe')
+    @chef = Chef.create!(chefname: 'mashrur', email: 'mashrur@example.com')
+    @recipe = @chef.recipes.build(name: 'vegetable', description: 'great vegetable recipe')
   end
 
   test 'recipe is valid' do
@@ -27,6 +28,11 @@ class RecipeTest < ActiveSupport::TestCase
 
   test 'description should not be more than 500 characters' do
     @recipe.description = 'a' * 501
+    assert_not @recipe.valid?
+  end
+
+  test 'recipe without chef should be invalid' do
+    @recipe.chef_id = nil
     assert_not @recipe.valid?
   end
 end
