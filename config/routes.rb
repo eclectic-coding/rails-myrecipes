3f0a1c24901b5a865d166cfae520440e0b83e940
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-31 13:56:29 -0400

    Finish gravatar setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :chefs
-  resources :recipes
   root 'pages#home'
   get 'pages/home', to: 'pages#home'
+
+  get '/recipes', to: 'recipes#index'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
 end
---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class RecipesTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(chefname: 'chuck', email: 'chuck@test.com')
+    @recipe = Recipe.create(name: 'vegetable saute',
+                            description: 'great vegetable saute, add vegetable and oil',
+                            chef: @chef)
+    @recipe2 = @chef.recipes.build(name: 'chicken saute',
+                                   description: 'great chicken dish')
+    @recipe2.save
+  end
+
+  test 'should get recipe index' do
+    get recipes_url
+    assert_response :success
+  end
+
+  test 'should get recipes listing' do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: @recipe.name
+    assert_select 'a[href=?]', recipe_path(@recipe2), text: @recipe2.name
+  end
+
+  test 'should get recipes show' do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
+    # assert_select 'a[href=?]', edit_recipe_path(@recipe), text: 'Edit this recipe'
+    # assert_select 'a[href=?]', recipe_path(@recipe), text: 'Delete this recipe'
+  end
+
+end
