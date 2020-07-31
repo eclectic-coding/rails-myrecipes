12b50011b26f0131553d78dcf75d0a25394dd965
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-31 18:52:39 -0400

    Finish recipes/new and flash messaging

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,12 +3,9 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @chef = Chef.create!(chefname: 'chuck', email: 'chuck@test.com')
-    @recipe = Recipe.create(name: 'vegetable saute',
-                            description: 'great vegetable saute, add vegetable and oil',
-                            chef: @chef)
-    @recipe2 = @chef.recipes.build(name: 'chicken saute',
-                                   description: 'great chicken dish')
+    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
+    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
     @recipe2.save
   end
 
@@ -20,18 +17,40 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test 'should get recipes listing' do
     get recipes_path
     assert_template 'recipes/index'
-    assert_select 'a[href=?]', recipe_path(@recipe), text: @recipe.name
-    assert_select 'a[href=?]', recipe_path(@recipe2), text: @recipe2.name
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
   end
 
-  test 'should get recipes show' do
+  test "should get recipes show" do
     get recipe_path(@recipe)
     assert_template 'recipes/show'
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
-    # assert_select 'a[href=?]', edit_recipe_path(@recipe), text: 'Edit this recipe'
-    # assert_select 'a[href=?]', recipe_path(@recipe), text: 'Delete this recipe'
+  end
+
+  test 'create new valid recipe submissions' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "lots of stuff"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe } }
+    end
+    follow_redirect!
+    assert_match name_of_recipe.titleize, response.body
+    assert_match name_of_recipe, response.body
+  end
+
+  test 'reject new valid recipe submissions' do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: ' ', description: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
 
 end
